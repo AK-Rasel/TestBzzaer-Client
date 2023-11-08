3ff172d5060db7757e793944cdbe3b0002f3841d
AK Rasel <raselak101010@gmail.com>
2023-11-08 21:09:37 +0600

    banner add

--- src/PrivetRouter/PrivetRouter.jsx
+++ src/PrivetRouter/PrivetRouter.jsx
@@ -1,6 +1,6 @@
 
 import { useContext } from "react";
-import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { AuthContext } from "../Auth/AuthProvider";
 
 
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,6 +9,8 @@ import 'swiper/css/navigation';
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Link } from 'react-router-dom';
+
 
 const Banner = () => {
   return (
@@ -16,7 +18,7 @@ const Banner = () => {
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
-        
+
         autoplay={{
           delay: 8000,
           disableOnInteraction: false,
@@ -25,29 +27,39 @@ const Banner = () => {
         // pagination={{
         //   clickable: true,
         // }}
+        
         navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper lg:h-[80vh]    "
+        modules={[ Autoplay,Pagination, Navigation]}
+        className="mySwiper  lg:h-[80vh]    "
       >
         <div className='relative'>
-          
-          <SwiperSlide><div>
-          <div className=' tracking-wide  z-10 absolute top-1/3 left-0 right-1/2 translate-x-1/4 bottom-0'><div><h1 className=' mb-6  lg:text-6xl  text-4xl font-Montserrat font-extrabold  text-white'>2024 Toyota <br /> <span>Land Cruiser</span></h1> <p className='  text-[#F0C543] text-lg uppercase tracking-widest font-semibold'>Caming Soon ...</p></div></div>
-            <img className=' object-cover h-[80vh] w-full   ' src="https://i.ibb.co/KFBTyJW/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay.jpg" alt="" />
-           
+          <SwiperSlide><div >
+          <img className=' object-cover h-[80vh] w-full   ' src="https://i.ibb.co/CM3xrpJ/Login-img.jpg" alt="" />
+            <div className='max-w-7xlxl    z-10 absolute top-1/3 inset-0 left-0 right-0 translate-x-1/4 bottom-0'><div><h1 className=' mb-6 w-1/2  lg:text-6xl  text-4xl font-Montserrat font-extrabold  text-white'>Welcome to <span>TasteBazaar</span></h1> <p className='  text-white mb-3 lg:text-lg lg:text-center  lg:font-semibold lg:w-1/3 w-1/2  '>Where Culinary Passion Meets Seamless Management! Explore a World of Efficiency and Flavorful Excellence.</p>
+            <Link to="/login" className='lg:flex lg:justify-center lg:w-2/6 mt-5 '> <button className='btn  bg-[#f56511] hover:bg-[#f56511] text-white outline-none border-none rounded-xl hover:rounded-full'>Join Us Today</button></Link>
+            </div>
+            
+            </div>
+            
+
           </div></SwiperSlide>
 
-          <SwiperSlide>
-            <div>
-          <div className=' tracking-wide  z-10 absolute top-1/3 left-0 right-1/2 translate-x-1/4 bottom-0'><div>
-            <h1 className=' mb-6 text-4xl lg:text-6xl font-Montserrat font-extrabold  text-white'>2023 BMW <span>ix </span></h1> <p className='  text-[#F0C543] text-lg uppercase tracking-widest font-semibold'>Now available have <span>now ...</span></p></div></div>
-          <img className=' bg-cover  w-full h-[80vh] lg:h-full  ' src="https://i.ibb.co/CM3xrpJ/Login-img.jpg" alt="" />
-            </div></SwiperSlide>
+
+          <SwiperSlide><div >
+            <div className=' font-Ubuntu space-y-1 tracking-wide  z-10 absolute top-1/3 left-0 right-1/2 translate-x-1/4 bottom-0'><div><h1 className=' mb-6  lg:text-6xl  text-3xl font-Montserrat font-extrabold  text-white'>Welcome to <br /> <span>TasteBazaar</span></h1> 
+            <Link to={'/all-food-items'}> <button className='btn  bg-[#f56511] hover:bg-[#f56511] text-white outline-none border-none rounded-xl hover:rounded-full mt-4'>Explore Menu</button></Link>
+            </div></div>
+            <img className=' object-cover h-[80vh] w-full   ' src="https://i.ibb.co/KFBTyJW/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay.jpg" alt="" />
+
+          </div></SwiperSlide>
+
+
+
         </div>
 
-        
-        
-        
+
+
+
 
       </Swiper>
     </div>
